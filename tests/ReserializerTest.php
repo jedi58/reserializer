76a57bcea0a2ba0da5359a44e5d93525ccf07238
David G. Paul <jedi58@users.noreply.github.com>
2016-02-19 21:03:11 +0000

    Added additional test cases
    - Test cases for string
    - Test cases for arrays

--- tests/ReserializerTest.php
+++ tests/ReserializerTest.php
@@ -14,6 +14,32 @@ class ReserializerTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testParse_Int()
 	{
-		$this->assertSame(32, Reserializer::parse('i:32'));
+		$this->assertSame(32, Reserializer::parse('i:32;'));
+	}
+
+	public function testParseString()
+	{
+		$this->assertSame('something', Reserializer::parse('s:9:"something";'));
+	}
+
+	public function testParseArray()
+	{
+		$this->assertSame(
+			array('a', 'b'),
+			Reserializer::parse(
+			'a:2:{i:0;s:1:"a";i:1;s:1:"b";}'
+		));
+	}
+
+	public function testParseAssociativeArray()
+	{
+		$this->assertSame(
+			array(
+				'a' => 'abc',
+				'b' => 'def'
+			),
+			Reserializer::parse(
+			'a:2:{s:1:"a";s:3:"abc";s:1:"b";s:3:"def";}'
+		));	
 	}
 }
